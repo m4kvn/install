13ae339e5058175e0338b162da97fc175d6c6f0a
m4kvn <fun.masahiro@gmail.com>
2018-03-12 20:43:48 +0900

    Update install script of nodejs

--- installer/etc/nodejs.sh
+++ installer/etc/nodejs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-version="v9.2.0"
-root="~/.nvm"
+version=v9.2.0
+root=~/.nvm
 
 if [ ! -s $root ]; then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 fi
 
 if [ ! -s $root/versions/node/$version ]; then
-    $root/nvm.sh install $version
+    . $root/nvm.sh
+    nvm install $version
 fi
