5ae09580f9904a5a483559e5b4a3ba9bfbc51ad7
m4kvn <fun.masahiro@gmail.com>
2018-03-12 11:14:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! $(xcode-select -p 2>/dev/null) ]; then
+    printf '`xcode-select --install` is not running'
+    exit 1
+fi
+
 github=https://github.com/m4kvn
 install=$github/install/raw/master
 
