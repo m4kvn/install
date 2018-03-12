684a7119cdcb98c1c340a7156ef1e1a2de40451d
m4kvn <fun.masahiro@gmail.com>
2018-03-12 15:42:04 +0900

    Fix install script of homebrew

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! $(xcode-select -p 2>/dev/null) ]; then
+if [[ ! $(xcode-select -p 2>/dev/null) ]]; then
     echo "unable to get active developer directory."
     echo "Please execute command \"xcode-select --install\""
     exit 1
@@ -8,7 +8,7 @@ fi
 
 homebrew=https://raw.githubusercontent.com/Homebrew
 
-if [ ! $(brew --version 2>/dev/null) ]; then
+if [[ ! $(brew --version 2>/dev/null) ]]; then
     /usr/bin/ruby -e "$(curl -fsSL $homebrew/install/master/install)"
     exec ${SHELL}
 fi
