4b39c7901c42bd69dac4661bdf8bca5269d928ff
m4kvn <fun.masahiro@gmail.com>
2018-03-29 23:46:55 +0900

    Create isntall script of node

---
+++ installer/etc/node.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+version=v9.10.0
+github=https://github.com/riywo
+root=~/.ndenv
+ndenv=$root/bin/ndenv
+
+if [ ! -s $root ]; then
+    git clone $github/rbenv.git $root
+fi
+
+if [ ! -s $root/plugins/node-build ]; then
+    git clone $github/node-build.git $root/plugins/node-build
+fi
+
+if [ ! -s $root/versions/$version ]; then
+    $ndenv install $version
+    $ndenv global $version
+    $ndenv rehash
+fi
